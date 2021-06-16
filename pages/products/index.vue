c5fa8a83bd4a2c115427d04965640516d4c82625
ntt19981812 <tientainguyen.coding@gmail.com>
2021-06-17 00:13:30 +0700

    component and route setting

---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo.slice(0, 1)"
+      :key="cardInfo.id"
+      :cards-section="cardInfo"
+    />
+    <SmallCardDisplay
+      v-for="cardInfo in smallCardSections"
+      :key="cardInfo.id"
+      :cards-section="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
